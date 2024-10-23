292a0eed11a318fcc652e689f86a16ceae8d2559
Peter Kolb <pekoli@gmail.com>
2024-10-23 17:39:13 +0200

    SpaceAfter-Annotierung erzeugt

--- not-to-release/scripts/detokenize.py
+++ not-to-release/scripts/detokenize.py
@@ -1,7 +1,39 @@
 import sys
 import os
-import random
 import re
+
+
+# source: https://github.com/stanfordnlp/stanza/blob/main/stanza/utils/datasets/common.py
+def read_sentences_from_conllu(filename):
+    """
+    Reads a conllu file as a list of list of strings
+
+    Finding a blank line separates the lists
+    """
+    sents = []
+    cache = []
+    with open(filename, encoding="utf-8") as infile:
+        for line in infile:
+            line = line.strip()
+            if len(line) == 0:
+                if len(cache) > 0:
+                    sents.append(cache)
+                    cache = []
+                continue
+            cache.append(line)
+        if len(cache) > 0:
+            sents.append(cache)
+    return sents
+
+def write_sentences_to_file(outfile, sents):
+    for lines in sents:
+        for line in lines:
+            print(line, file=outfile)
+        print("", file=outfile)
+
+def write_sentences_to_conllu(filename, sents):
+    with open(filename, 'w', encoding="utf-8") as outfile:
+        write_sentences_to_file(outfile, sents)
 
 
 def detokenize(text):
@@ -43,45 +75,63 @@ def detokenize(text):
     return text
 
 
-def insert_line_breaks(text):
-    tokens = re.split(r"\s+", text)
-    neu = []
-    for token in tokens:
-        neu.append(token)
-        n = random.random()
-        if n <= 0.01:
-            neu.append("\n")
-        else:
-            neu.append(" ")
-    neu.pop()  # letztes "\n" oder " " wieder entfernen
-
-    # am Ende entweder "\n" oder " " anhängen
-    is_ueberschrift = False
-    if re.search(r"[\.\?!]$", neu[-1]) is None:
-        is_ueberschrift = True
-        neu.insert(0, "\n")  # Überschrift steht allein auf Zeile
-    n = random.random()
-    if n <= 0.1 or is_ueberschrift:
-        neu.append("\n")
+def handle_line(line, text, pieces, start, idx, sent):
+    t = text.find(pieces[1], start)
+    if t == -1:
+        print(f"{pieces[1]} NOT IN {text}")
+        print(f"\t=> {line}")
     else:
-        neu.append(" ")
-    return ''.join(neu)
+        start = t+len(pieces[1])
+    if idx < (len(sent)-1):
+        try:
+            if text[t+len(pieces[1])] != " ":
+                if pieces[-1] == "_":
+                    pieces[-1] = "SpaceAfter=no"
+                else:
+                    pieces[-1] += "|SpaceAfter=no"
+        except IndexError as err:
+            print(f"t={t}, start={start}, idx={idx}, pieces[1]={pieces[1]}, len(sent)={len(sent)}")
+            print(f"len(text)={len(text)}, len(pieces[1])={len(pieces[1])}")
+            print(f"text={text}")
+            raise err
+    return start
 
 
 def process_file(conllu_file, out_file):
-    with open(conllu_file, "rt", encoding="utf-8") as conllu, open(out_file, "wt", encoding="utf-8") as out:
-        i = 0
-        for line in conllu:
+    mwl_pat = re.compile(r"^([0-9]+)-([0-9]+)\t")
+    sents = read_sentences_from_conllu(conllu_file)
+    i = 0
+    for sent in sents:
+        done_idx = []
+        start = 0
+        for idx, line in enumerate(sent):
             if line.startswith("# text = "):
                 line = line.strip()
                 text = line[9:]
                 text = detokenize(text)
-                #text = insert_line_breaks(text)
-                out.write("# text = "+text+"\n")
+                sent[idx] = "# text = "+text
                 i += 1
+            elif line.startswith("#"):
+                continue
+            elif mwl_pat.match(line):
+                pieces = line.split("\t")
+                [idx_from, idx_to] = pieces[0].split("-")
+                idx_from = int(idx_from)
+                idx_to = int(idx_to)
+                for x in range(idx_from, idx_to+1):
+                    done_idx.append(x)
+                start = handle_line(line, text, pieces, start, idx, sent)
+                sent[idx] = "\t".join(pieces)
             else:
-                out.write(line)
-    print("\t"+str(i)+" Sätze.")
+                pieces = line.split("\t")
+                if int(pieces[0]) in done_idx:
+                    continue
+                else:
+                    start = handle_line(line, text, pieces, start, idx, sent)
+                    sent[idx] = "\t".join(pieces)
+
+    write_sentences_to_conllu(out_file, sents)
+    print(f"\t{i} Sätze.")
 
 
 if __name__ == "__main__":
